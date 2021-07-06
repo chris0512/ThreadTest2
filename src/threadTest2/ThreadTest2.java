249fccfc189b8abd9d831430c4cefe3bff2b3871
Chris0512 <kyang3200@gmail.com>
2021-07-06 09:01:52 +0800

    set thread priority and show id, name, state, alive

--- src/threadTest2/ThreadTest2.java
+++ src/threadTest2/ThreadTest2.java
@@ -1,15 +1,25 @@
 package threadTest2;
 
-class MyRun implements Runnable
+class MyThread extends Thread
 {
-    public void run(){}
+    public MyThread(String name)
+    {
+        super(name);
+        setPriority(Thread.MIN_PRIORITY + 2);
+    }
 }
-
 
 public class ThreadTest2
 {
     public static void main(String[] args)throws Exception
     {
-        Thread t = new Thread(new MyRun(),"My Name");
+        MyThread t = new MyThread("My thread 1");
+
+        System.out.println("ID " + t.getId());
+        System.out.println("Name " + t.getName());
+        System.out.println("Priority " + t.getPriority());
+        t.start();
+        System.out.println("State " + t.getState());
+        System.out.println("Alive " + t.isAlive());
     }
 }
